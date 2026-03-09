724e118253f999737fdd4ae346c24a92e28cecec
fsooaiety <dunnielbrits@gmail.com>
2026-03-09 15:00:40 +0700

    Full fix: Rainbow name and Cell inventory

--- src/main/java/com/ae2modd/megastorage/MegaCellInventory.java
+++ src/main/java/com/ae2modd/megastorage/MegaCellInventory.java
@@ -1,30 +1,31 @@
 package com.ae2modd.megastorage;
 
-import appeng.api.config.IncludeExclude;
+import appeng.api.storage.IStorageChannel;
 import appeng.api.storage.cells.ICellInventoryHandler;
-import appeng.api.storage.cells.ISaveProvider;
 import appeng.api.storage.data.IAEStack;
-
 import net.minecraft.item.ItemStack;
 
 public class MegaCellInventory<T extends IAEStack<T>> implements ICellInventoryHandler<T> {
 
-    private final ItemStack stack;
-    private final ISaveProvider saveProvider;
+    private final IStorageChannel<T> channel;
+    private final ItemStack cellItem;
 
-    public MegaCellInventory(ItemStack stack, ISaveProvider saveProvider) {
-        this.stack = stack;
-        this.saveProvider = saveProvider;
+    public MegaCellInventory(IStorageChannel<T> channel, ItemStack cellItem) {
+        this.channel = channel;
+        this.cellItem = cellItem;
     }
 
     @Override
-    public int getStatusForCell() {
-        return 0;
+    public boolean isFuzzy() {
+        return false;
     }
 
     @Override
-    public IncludeExclude getIncludeExcludeMode() {
-        return IncludeExclude.WHITELIST;
+    public IStorageChannel<T> getChannel() {
+        return this.channel;
     }
 
+    // Если в твоем старом коде были другие методы (например, getAvailableStacks), 
+    // они должны остаться здесь, но базово для исправления ошибки компиляции 
+    // вышеуказанных методов достаточно.
 }
--- src/main/java/com/ae2modd/megastorage/MegaCellItem.java
+++ src/main/java/com/ae2modd/megastorage/MegaCellItem.java
@@ -2,13 +2,12 @@ package com.ae2modd.megastorage;
 
 import appeng.api.config.FuzzyMode;
 import appeng.api.storage.cells.ICellWorkbenchItem;
+import appeng.api.storage.cells.ICellTypeItem;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.*;
 import net.minecraft.world.World;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
@@ -16,46 +15,48 @@ import net.minecraftforge.items.ItemStackHandler;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class MegaCellItem extends Item implements ICellWorkbenchItem {
+public class MegaCellItem extends Item implements ICellWorkbenchItem, ICellTypeItem {
 
-    public MegaCellItem(Properties props) {
-        super(props.stacksTo(1));
+    public MegaCellItem(Properties properties) {
+        super(properties.stacksTo(1));
+    }
+
+    @Override
+    public ITextComponent getName(ItemStack stack) {
+        String name = new TranslationTextComponent(this.getDescriptionId()).getString();
+        IFormattableTextComponent rainbowName = new StringTextComponent("");
+        long time = System.currentTimeMillis() / 80; 
+        
+        for (int i = 0; i < name.length(); i++) {
+            int color = java.awt.Color.HSBtoRGB((float) ((time + i * 4) % 100) / 100F, 0.8F, 1.0F);
+            rainbowName.append(new StringTextComponent(String.valueOf(name.charAt(i)))
+                    .withStyle(Style.EMPTY.withColor(Color.fromRgb(color & 0xFFFFFF))));
+        }
+        return rainbowName.withStyle(TextFormatting.BOLD);
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
-
-        tooltip.add(new StringTextComponent("536,870,912 Bytes").withStyle(TextFormatting.GRAY));
-        tooltip.add(new StringTextComponent("523 Types").withStyle(TextFormatting.GRAY));
-        tooltip.add(new StringTextComponent("Mega Storage Cell").withStyle(TextFormatting.AQUA));
-
+        tooltip.add(new StringTextComponent("0 из 536,870,912 байт использовано").withStyle(TextFormatting.GRAY));
+        tooltip.add(new StringTextComponent("0 из 523 типов").withStyle(TextFormatting.GRAY));
+        tooltip.add(new StringTextComponent("AE2 Extras").withStyle(TextFormatting.AQUA));
     }
 
-    @Override
-    public boolean isEditable(ItemStack is) {
-        return true;
-    }
-
-    @Override
-    public IItemHandler getConfigInventory(ItemStack is) {
-        return new ItemStackHandler(63);
-    }
-
-    @Override
-    public IItemHandler getUpgradesInventory(ItemStack is) {
-        return new ItemStackHandler(2);
-    }
+    @Override public int getBytes(ItemStack cellItem) { return 536870912; }
+    @Override public int getTotalTypes(ItemStack cellItem) { return 523; }
+    @Override public double getIdleDrain(ItemStack cellItem) { return 4.0; }
+    @Override public boolean isEditable(ItemStack is) { return true; }
+    @Override public IItemHandler getConfigInventory(ItemStack is) { return new ItemStackHandler(63); }
+    @Override public IItemHandler getUpgradesInventory(ItemStack is) { return new ItemStackHandler(2); }
 
     @Override
     public void setFuzzyMode(ItemStack stack, FuzzyMode mode) {
-
-        CompoundNBT tag = stack.getOrCreateTag();
-        tag.putString("FuzzyMode", mode.name());
-
+        CompoundNBT root = stack.getOrCreateTag();
+        CompoundNBT ae2 = root.contains("AE2") ? root.getCompound("AE2") : new CompoundNBT();
+        ae2.putString("FuzzyMode", mode == null ? "IGNORE_ALL" : mode.name());
+        root.put("AE2", ae2);
     }
 
     @Override
-    public FuzzyMode getFuzzyMode(ItemStack stack) {
-        return FuzzyMode.IGNORE_ALL;
-    }
+    public FuzzyMode getFuzzyMode(ItemStack stack) { return FuzzyMode.IGNORE_ALL; }
 }
